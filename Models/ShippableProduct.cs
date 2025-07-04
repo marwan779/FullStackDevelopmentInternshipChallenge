4ac1e1523a09b3a4f5f6fd85f6ba7fc26394e0bc
Marwan <UGS.16220338@eng.suez.edu.eg>
2025-07-04 20:15:26 +0300

    Adding ShippableProduct

---
+++ Models/ShippableProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FawryChallenge.Models
+{
+    public class ShippableProduct : BaseProduct
+    {
+        public double Weight { get; set; }
+        public ShippableProduct(string name, int quantity, double price, double weight)
+            : base(name, quantity, price)
+        {
+            Weight = weight;
+        }
+
+
+
+    }
+}
