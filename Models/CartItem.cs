e022087b65b9c69e7636c9485a42fd33418d717a
Marwan <UGS.16220338@eng.suez.edu.eg>
2025-07-04 20:38:58 +0300

    Adding CartItem class

---
+++ Models/CartItem.cs
@@ -0,0 +1,16 @@
+﻿namespace FawryChallenge.Models
+{
+    public class CartItem
+    {
+        BaseProduct Product;
+        public int Amount { get; set; }
+        public CartItem(BaseProduct product, int amount)
+        {
+            Product = product;
+            Amount = amount;
+        }
+
+        //Calculate the total price of the cart Item
+        public double ItemPrice() => Product.Price * Amount;
+    }
+}
---
+++ Models/ExpirableShippableProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FawryChallenge.Models
+{
+    public class ExpirableShippableProduct : ExpirableProduct
+    {
+        public double Weight { get; set; }
+        public ExpirableShippableProduct(string name, int quantity, double price, double weight, DateOnly expirationDate)
+            : base(name, quantity, price, expirationDate)
+        {
+            Weight = weight;
+        }
+    }
+}
