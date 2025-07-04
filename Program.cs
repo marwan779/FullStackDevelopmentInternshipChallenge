c2bc1057f1df6497382ff69c6d0c1fb57518dbd3
Marwan <UGS.16220338@eng.suez.edu.eg>
2025-07-04 22:55:02 +0300

    Start Testing

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace FawryChallenge
+﻿using FawryChallenge.Models;
+
+namespace FawryChallenge
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Customer customer = new Customer("Marwan Mohamed", 1000);
+
+            var cheese = new ExpirableShippableProduct("Cheese", 100, 5, 400, DateOnly.FromDateTime(DateTime.Today.AddDays(1)));
+            var biscuits = new ExpirableProduct("Biscuits", 150, 2, DateOnly.FromDateTime(DateTime.Today.AddDays(2)));
+            var tv = new ShippableProduct("TV", 100, 3, 700);
+            var scratchCard = new BaseProduct("Scratch Card", 50, 10);
+
+
         }
     }
 }
