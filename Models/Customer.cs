0c8e52e6d46c8a1322fe1d056608c9bd557ead0b
Marwan <UGS.16220338@eng.suez.edu.eg>
2025-07-04 20:56:46 +0300

    Adding Customer model

---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FawryChallenge.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; } = string.Empty;
+        public double Balance { get; set; }
+        public Customer(string name, double balance)
+        {
+            Name = name;
+            Balance = balance;
+        }
+    }
+}
